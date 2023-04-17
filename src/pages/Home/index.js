a4354b826c3b7510f7edbadaa5b80d6318a302b3
Renan Jefferson Rodrigues Silva <renanjefferson.silva@gmail.com>
2023-04-17 16:32:25 -0300

    Buscando um filme através da barra de pesquisa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,30 @@ const api_base_url_image = 'https://image.tmdb.org/t/p';
 const api_key = '4739517f5c4a7ee3075aa7e1c84adeff';
 const moviesContainer = document.getElementById('movies');
 
+const inputSearch = document.getElementById('input-search');
+const buttonSearch = document.getElementById('search-button');
+buttonSearch.addEventListener('click', searchMovie);
+
+
+async function searchMovie() { 
+  if(inputSearch.value !== null) {
+    clearAllMovies();
+    const movies = await searchMovieByName(inputSearch.value);
+    movies.map(movie => renderMovie(movie));
+  }
+}
+
+function clearAllMovies() {
+  moviesContainer.innerHTML = '';
+}
+
+async function searchMovieByName(title) {
+  const url = `${api_base_url}/search/movie?api_key=${api_key}&language=pt-BR&query=${title}&page=1&include_adult=false`;
+  const response = await fetch(url);
+  const { results } = await response.json();
+  return results;
+}
+
 async function getPopularMovies() {
   const url = `${api_base_url}/movie/popular?api_key=${api_key}&language=pt-BR&page=1`;
   const response = await fetch(url);
@@ -11,8 +35,8 @@ async function getPopularMovies() {
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const movies = await getPopularMovies();
-  movies.map((movie) => renderMovie(movie))
+  const movies =  await getPopularMovies();
+  movies.map(movie => renderMovie(movie))
 });
 
 function renderMovie(movie) {
@@ -20,7 +44,7 @@ function renderMovie(movie) {
     <div class="movie-container">
       <div class='movie-details'>
         <div class='movie-image'>
-          <img src='${api_base_url_image}/w200${movie.poster_path}' alt='Imagem ilustrativa'>
+        ${movie.poster_path ? `<img src='${api_base_url_image}/w200${movie.poster_path}' alt='Imagem ilustrativa'>` : ''}          
         </div>
         <div class='movie-information'>
           <h2>${movie.title}</h2>
